fee8579360faa57165f2953378cc9b9109680e7a
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-03-31 12:04:31 -0300

    pagina_cursojs_2.0

---
+++ aula14/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+var d = 1
+
+while ( c <= 5) {
+    console.log(`Contando a letra C ${c}`)
+    c++
+}
+
+
+do {
+    console.log(`Contando a letra D ${d}`)
+    d++
+} while ( d <= 3 )
+
+
+for(var a = 1; a <= 5; a++){
+    console.log(`Contando a letra A ${a}`)
+}
+
+console.log(`Vai começar...`)
+for(var b = 1; b <= 4 ; b++){
+    console.log(`Contando a letra B ${b}`)
+}
+console.log(`FIM!`)
