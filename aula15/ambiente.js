b8eb7ccdb6f3cacbf5e3ffc91c13eb1689286000
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-02 17:04:38 -0300

    pagina_cursojs_2.4

---
+++ aula15/ambiente.js
@@ -0,0 +1,17 @@
+let num = [5,8,9,3]
+
+
+num.sort() // Colocando em ordem crescente
+num[3] = 6 // Mudando um elemento
+num.push(7) // Adicionando elemento
+
+num.length
+
+
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}}`)
+
+
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
